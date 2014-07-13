3bf0907b377f425df792d8c4181a12f74df5a14e
SeoRoman <roman.lopez@roosterteeth.com>
2014-07-13 00:10:41 -0500

    changed to length property of json returned object

--- public/assets/js/controllers/appControllers.js
+++ public/assets/js/controllers/appControllers.js
@@ -26,13 +26,8 @@ angular.module('app.controllers', [])
     OfficeService.getAll();
 
     $scope.getNumberOfOffices = function() {
-      var count = 0;
-
-      angular.forEach($rootScope.offices, function(office) {
-        count += office.name ? 1 : 0;
-      });
-
-      return count;
+      var offices = $rootScope.offices;
+      return offices.length;
     }
     //$scope.offices = OfficeService.getAll();
 
